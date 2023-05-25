0ca80ba78d0b43114872292f416d0f9129dcbc94
julianaoliveiras <julianaoliveirasto@gmail.com>
2023-05-25 15:11:48 -0300

    connect to slack

---
+++ assets/bootstrap.js
@@ -0,0 +1,34 @@
+// Bootstrap the app when the DOM is ready
+(function () {
+    // Initialize the Zendesk JavaScript API client
+    // https://developer.zendesk.com/apps/docs/developer-guide/getting_started
+    window.client = ZAFClient.init();
+
+    // OAuth flow kick-off requires a subdomain
+    let subdomain = undefined;
+
+    // Integration name is the unique identifier for your integration app
+    let integrationName = undefined;
+
+    client
+        .context()
+        .then(function (context) {
+            // Derive subdomain & integration name
+            subdomain = context["account"]["subdomain"];
+            integrationName = "dev_zis_gol";
+        })
+        .then(function () {
+            // Bind button to start a Zendesk OAuth flow
+            document
+                .getElementById("btnConnect")
+                .addEventListener("click", function () {
+                    startOAuth(integrationName, subdomain);
+                });
+            // Bind button to start a Slack OAuth flow
+            document
+                .getElementById("btnConnectSlack")
+                .addEventListener("click", function () {
+                    startSlackOAuth(integrationName, subdomain);
+                });
+        });
+})();
